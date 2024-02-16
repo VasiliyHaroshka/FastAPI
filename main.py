23c4cd4bcb958993323bebf6c99eac4405e3ce96
VasiliyHaroshka <horvasiliy@mail.ru>
2024-02-16 15:21:02 +0300

    add black

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import uvicorn
 from fastapi import FastAPI
 
 from user import routers as user_router
@@ -5,3 +6,6 @@ from user import routers as user_router
 app = FastAPI()
 
 app.include_router(user_router.router)
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", reload=True)
